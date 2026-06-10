3eb2a801aa1c0b339619a3dd2d27d668698edf11
phiona-dev <phionadev@gmail.com>
2026-06-10 21:43:14 +0300

    demoproducts json

--- frontend/src/pages/demoPage.jsx
+++ frontend/src/pages/demoPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import demoData from "../data/demoProducts.json";
 const demoPage = () => {
+
+  const demoProducts = demoData.products;
   return (
-    <div>demoPage</div>
+    <div>
+      {demoProducts.map(product => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.brand}</p>
+          <p>{product.barcode}</p>
+          <p>{product.category}</p>
+          <p>{product.is_counterfeit}</p>
+          <p>{product.notes}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
